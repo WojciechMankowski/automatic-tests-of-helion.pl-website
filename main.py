1eb50accc1e6d3c664c8b1720e7162afd46d5e91
Wojciech Mankowski <wojtekm510@gmail.com>
2022-11-17 16:45:31 +0100

    add new test

--- main.py
+++ main.py
@@ -31,13 +31,38 @@ class MainTests(unittest.TestCase):
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         driver.get("https://www.helion.pl")
         driver.implicitly_wait(5)
-        previous_title = driver.title
         search = driver.find_element(By.ID, 'inputSearch')
         search.click()
         search.send_keys("python")
-        search.send_keys(Keys.ENTER)
-        sleep(6)
-        title = driver.title
-        assert previous_title == title
-        driver.quit()
+        sleep(10)
+        element = driver.find_element(By.CSS_SELECTOR, 'li.wszystkie')
+        button = element.find_element(By.CSS_SELECTOR, "p.button").find_element(By.TAG_NAME, "a")
+        driver.get(button.get_attribute('href'))
+        assert driver.title == '"python" - Wyniki wyszukiwania - Wydawnictwo Helion, księgarnia helion.pl'
+    def test_select_categpry(self):
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get("https://helion.pl/search?szukaj=python")
+        driver.implicitly_wait(5)
+        panle_category = driver.find_element(By.CLASS_NAME, 'sub-categories')
+        list_elements = panle_category.find_elements(By.TAG_NAME, "li")
+        selected_item = list_elements[1].find_element(By.TAG_NAME, "h4").find_element(By.TAG_NAME, "a")
+        driver.get(selected_item.get_attribute("href"))
+        panle_category = driver.find_element(By.CLASS_NAME, 'sub-categories')
+        list_elements = panle_category.find_elements(By.TAG_NAME, "li")
+        selected_item = list_elements[0].find_element(By.TAG_NAME, "h4").find_element(By.TAG_NAME, "a")
+        assert selected_item.get_attribute('title') == ".NET - Programowanie"
+    def test_seleced_format(self):
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        driver.get("https://helion.pl/search?szukaj=python")
+        driver.implicitly_wait(5)
+        panle_category = driver.find_element(By.CLASS_NAME, 'sub-categories')
+        list_elements = panle_category.find_elements(By.TAG_NAME, "li")
+        selected_item = list_elements[1].find_element(By.TAG_NAME, "h4").find_element(By.TAG_NAME, "a")
+        driver.get(selected_item.get_attribute("href"))
+        panle_category = driver.find_element(By.CLASS_NAME, 'sub-categories')
+        list_elements = panle_category.find_elements(By.TAG_NAME, "li")
+        selected_item = list_elements[0].find_element(By.TAG_NAME, "h4").find_element(By.TAG_NAME, "a")
+        driver.get(selected_item.get_attribute('href'))
+        print(driver.current_url)
+        assert driver.current_url == 'https://helion.pl/search/?szukaj=python&wsprzed=1&formaty=p&ceny=-'
 # driver.close()
--- main2.py
+++ main2.py
@@ -3,14 +3,20 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import  ActionChains
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get("https://www.helion.pl")
+driver.get("https://helion.pl/search?szukaj=python")
+driver.maximize_window()
 driver.implicitly_wait(5)
-search = driver.find_element(By.ID, 'inputSearch')
-search.click()
-# search.send_keys("python")
-search.send_keys(Keys.ENTER)
-sleep(6)
-driver.close()
+form = driver.find_element(By.CLASS_NAME, 'filter-form')
+list_format = form.find_element(By.ID, "filtrFormat").find_elements(By.CLASS_NAME, "checkbox-line")
+label_format = list_format[2].find_element(By.TAG_NAME, 'label')
+checbox =  list_format[2].find_element(By.TAG_NAME, 'input')
+print(checbox.get_attribute("id"))
+driver.execute_script("""
+    
+""")
+
+# driver.execute_script("document.getElementById('scope-playlist-modify-public').click()")
